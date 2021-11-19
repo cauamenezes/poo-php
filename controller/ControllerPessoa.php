7f28408ef52dd732b5ae515620a554cf000a6b9e
cauamenezes <cauamenezes.07@gmail.com>
2021-11-19 11:28:15 -0300

    create, findById e findAll feitos

--- controller/ControllerPessoa.php
+++ controller/ControllerPessoa.php
@@ -4,21 +4,44 @@ class ControllerPessoa {
 
     private $_method;
     private $_modelPessoa;
+    private $_codPessoa;
 
     public function __construct($model) {
         
         $this->_modelPessoa = $model;
         $this->_method = $_SERVER['REQUEST_METHOD'];
 
+        //PERMITE RECEBER DADOS JSON ATRAVÉS DA REQUISIÇÃO
+        $json = file_get_contents("php://input");
+        $dadosPessoa = json_decode($json);
+
+        $this->_codPessoa = $dadosPessoa->cod_pessoa ?? null;
+
     }
 
     function router() {
 
         switch ($this->_method) {
-            case 'value':
-                # code...
+            case 'GET':
+
+                if(isset($this->_codPessoa)) {
+                    return $this->_modelPessoa->findById();    
+                }
+                return $this->_modelPessoa->findAll();
                 break;
             
+
+            case 'POST':
+                return $this->_modelPessoa->create();
+                break;
+
+            case 'PUT':
+                # code...
+                break;
+
+            case 'DELETE':
+                # code...
+                break;
             default:
                 # code...
                 break;
--- model/ModelPessoa.php
+++ model/ModelPessoa.php
@@ -3,8 +3,27 @@
 class ModelPessoa{
 
     private $_conn;
+    private $_codPessoa;
+    private $_nome;
+    private $_sobrenome;
+    private $_email;
+    private $_celular;
+    private $_fotografia;
 
     public function __construct($conn) {
+
+        //PERMITE RECEBER DADOS JSON ATRAVÉS DA REQUISIÇÃO
+        $json = file_get_contents("php://input");
+        $dadosPessoa = json_decode($json);
+
+        //RECEBIMENTO DOS DADOS DO POSTMAN:
+        $this->_codPessoa = $dadosPessoa->cod_pessoa ?? null;
+        $this->_nome = $dadosPessoa->nome ?? null;
+        $this->_sobrenome = $dadosPessoa->sobrenome ?? null;
+        $this->_email = $dadosPessoa->email ?? null;
+        $this->_celular = $dadosPessoa->celular ?? null;
+        $this->_fotografia = $dadosPessoa->fotografia ?? null;
+
         $this->_conn = $conn;
     }
 
@@ -20,8 +39,42 @@ class ModelPessoa{
         $statement->execute();
 
         //DEVOLVE OS VALORES DA SELECT PARA SEREM UTILIZADOS
-        return $statement->fetchAll();
+        return $statement->fetchAll(\PDO::FETCH_ASSOC);
 
+    }
+
+    public function findById() {
+
+        $sql = "SELECT * FROM tbl_pessoa WHERE cod_pessoa = ?";
+
+        $statement = $this->_conn->prepare($sql);
+        $statement->bindValue(1, $this->_codPessoa);
+
+        $statement->execute();
+
+        return $statement->fetchAll(\PDO::FETCH_ASSOC);
+    }
+
+    public function create() {
+
+        $sql = "INSERT INTO tbl_pessoa(nome, sobrenome, email, celular, fotografia)
+                VALUES (?, ?, ?, ?, ?)";
+
+        $statement = $this->_conn->prepare($sql);
+
+        $statement->bindValue(1, $this->_nome);
+        $statement->bindValue(2, $this->_sobrenome);
+        $statement->bindValue(3, $this->_email);
+        $statement->bindValue(4, $this->_celular);
+        $statement->bindValue(5, $this->_fotografia);
+
+        $statement->execute();
+
+        if ($statement->execute()) {
+            return "Success";
+        } else {
+            return "Error";
+        }
     }
 
 }
